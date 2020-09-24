ce9b682e5d0a452ae1182d19c07dd480a49adb9f
Eric Lau <ericlaucmo@gmail.com>
2020-09-24 01:08:32 -0700

    FEAT: Building and tooling Home view

--- src/renderer/components/Home.js
+++ src/renderer/components/Home.js
@@ -3,9 +3,25 @@ import React from 'react';
 export const Home = () => {
   return (
     <div>
-      <div>
-        <p>Bosses</p>
-        <p>Quests</p>
+      <div className="grid grid-cols-2 items-stretch">
+        <p
+          // TODO: Implement tooling for CSS-in-JS for TailwindCSS
+          className="inline-block text-center border border-red-500"
+          onClick={() => {
+            console.log('Clicked Bosses');
+          }}
+        >
+          Bosses
+        </p>
+        <p
+          // TODO: Implement tooling for CSS-in-JS for TailwindCSS
+          className="inline-block text-center border border-red-500"
+          onClick={() => {
+            console.log('Clicked Quests');
+          }}
+        >
+          Quests
+        </p>
       </div>
     </div>
   );
