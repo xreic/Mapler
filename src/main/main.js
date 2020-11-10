f3f5cc6787c59881da05f47219cef62d637b4263
Eric Lau <ericlaucmo@gmail.com>
2020-11-09 18:33:06 -0800

    sizing

--- src/main/main.js
+++ src/main/main.js
@@ -45,7 +45,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 350,
-    height: hideMenu ? 377 : 397,
+    height: hideMenu ? 367 : 397,
     resizable: !hideMenu,
     webPreferences: {
       enableRemoteModule: true,
