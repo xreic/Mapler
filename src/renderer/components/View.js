2f506e26390132bae805254687e52d3821dd302b
Eric Lau <ericlaucmo@gmail.com>
2020-11-16 18:36:11 -0800

    cleaning

--- src/renderer/components/View.js
+++ src/renderer/components/View.js
@@ -1,5 +1,5 @@
 // Core
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from '@reach/router';
 import {
   differenceInSeconds,
